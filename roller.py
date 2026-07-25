ff0720bbe9d054cbbdcda870745474bcf779bf1c
salemisaacbradley-max <salemisaacbradley@gmail.com>
2026-07-25 12:28:50 -0400

    Built Barebones dice roller

---
+++ roller.py
@@ -0,0 +1,48 @@
+#d20, d100, d12, d10, d8, d6, d4, d2 and d* should be a class
+import random
+class Die:
+    def __init__ (self, sides, modifier):
+        if sides > 2:
+            raise ValueError ("A die must have at least two sides")
+        elif type(sides) != int:
+            raise ValueError("Number of sides must be an integer")
+        elif type(modifier) != int:
+            raise ValueError("Modifier must be an Integer")
+        self.sides = sides
+        self.mod = modifier
+        self.value = None
+
+    def roll(self):
+        self.value = random.radint(1 + self.mod, self.sides + self.mod)
+        return self.value
+
+    def __repr__(self):
+        return f"D{self.sides} rolled {self.value}"
+
+class Cup:
+    def __init__(self, dice_set: list[Die]):
+        self.dice = dice_set
+
+    def roll_all(self):
+        return [die.roll() for die in self.dice]
+
+    def add_die(self, die) -> None:
+        self.dice.append(die)
+
+    def remove_die(self, die: Die) -> None:
+        if len(self.dice) <= 0:
+            raise ValueError("No dice to remove")
+        else:
+            print(f"Removing {die}")
+            if die not in self.dice:
+                print(f"No die matching {die} was found")
+            else:
+                self.dice.remove(die)
+                print(f"Removed {die}")
+
+    def get_total(self):
+        if any(die.value is None for die in self.dice):
+            self.roll_all()
+        return sum(die.value for die in self.dice)
+
+    
